03820dc74d94d1f5451fcc059182d81f4bf1bf69
Mohammad Kahal <>
2020-10-19 08:44:31 +0100

    First commit

--- vrp_project/vrp_project/vrp_project/settings.py
+++ vrp_project/vrp_project/vrp_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
-    # 3rd-party apps
+    # all 3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
     'allauth',
